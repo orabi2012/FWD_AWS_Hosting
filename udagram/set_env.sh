7d6cc420e2317fa754365e375c0b7d77a5c9c624
Ahmed Orabi <aorabi@outlook.com>
2022-07-02 16:21:33 +0300

    packages

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-fwd.crxbf58ewsyb.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=fwd3db
+export AWS_BUCKET=arn:aws:s3:::fwd-07-2022
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://localhost:8100 || http://fwd-07-2022.s3-website-us-east-1.amazonaws.com/
