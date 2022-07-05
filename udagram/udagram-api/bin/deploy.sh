103842d017cba2b795894cceb52bd57876c7d5f6
Ahmed Orabi <aorabi@outlook.com>
2022-07-05 22:35:49 +0300

    sectet

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,17 +1,18 @@
 npm run build
 eb init udagram-api --region us-east-1 --platform node.js
 eb use udagram-api-env
-eb use $POSTGRES_USERNAME
-eb use $POSTGRES_PASSWORD
-eb use $POSTGRES_HOST
-eb use $DB_PORT
-eb use $PORT
-eb use $POSTGRES_DB
-eb use $AWS_REGION
-eb use $AWS_PROFILE
-eb use $JWT_SECRET
-eb use $URL
-eb use $AWS_ACCESS_KEY_ID
-eb use $AWS_SECRET_ACCESS_KEY
+# eb use $POSTGRES_USERNAME
+# eb use $POSTGRES_PASSWORD
+# eb use $POSTGRES_HOST
+# eb use $DB_PORT
+# eb use $PORT
+# eb use $POSTGRES_DB
+# eb use $AWS_REGION
+# eb use $AWS_PROFILE
+# eb use $JWT_SECRET
+# eb use $URL
+# eb use $AWS_ACCESS_KEY_ID
+# eb use $AWS_SECRET_ACCESS_KEY
+# eb use $AWS_DEFAULT_REGION
 eb deploy udagram-api-env
 
