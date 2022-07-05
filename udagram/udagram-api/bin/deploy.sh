89fbae955e8e6761738717fcccbb0233b0f28d64
Ahmed Orabi <aorabi@outlook.com>
2022-07-05 22:19:05 +0300

    sectet

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,18 +1,17 @@
 npm run build
 eb init udagram-api --region us-east-1 --platform node.js
 eb use udagram-api-env
-eb setenv POSTGRES_USERNAME=postgres
-eb setenv POSTGRES_PASSWORD=postgres
-eb setenv POSTGRES_HOST=database-fwd.crxbf58ewsyb.us-east-1.rds.amazonaws.com
-eb setenv DB_PORT=5432
-eb setenv PORT=8080
-eb setenv POSTGRES_DB=fwd3db
-eb setenv AWS_BUCKET=arn:aws:s3:::fwd-07-2022
-eb setenv AWS_REGION=us-east-1
-eb setenv AWS_PROFILE=default
-eb setenv JWT_SECRET=mysecretstring
-eb setenv URL=http://localhost:8080 || http://fwd-07-2022.s3-website-us-east-1.amazonaws.com/
-eb setenv AWS_ACCESS_KEY_ID=AKIAX6AM2ZFXGRUE25M3
-eb setenv AWS_SECRET_ACCESS_KEY=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
+eb use $POSTGRES_USERNAME
+eb use $POSTGRES_PASSWORD
+eb use $POSTGRES_HOST
+eb use $DB_PORT
+eb use $PORT
+eb use $POSTGRES_DB
+eb use $AWS_REGION
+eb use $AWS_PROFILE
+eb use $JWT_SECRET
+eb use $URL
+eb use $AWS_ACCESS_KEY_ID
+eb use $AWS_SECRET_ACCESS_KEY
 eb deploy udagram-api-env
 
