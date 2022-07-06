1de5a443f029286da7872cbf7fcdf0c90c045b64
Ahmed Orabi <aorabi@outlook.com>
2022-07-07 01:58:43 +0300

    deploy_local

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 npm run build
-eb init udagram-api --region us-east-1 --platform node.js
+# eb init udagram-api --region us-east-1 --platform node.js
 eb use udagram-api-env
 # eb use $POSTGRES_USERNAME
 # eb use $POSTGRES_PASSWORD
